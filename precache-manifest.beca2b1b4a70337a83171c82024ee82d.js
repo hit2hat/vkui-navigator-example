01b7d4db1e67cd2a4bd0f51442f992fc1c4dc289
hit2hat <hit2hat@icloud.com>
2019-10-15 11:33:50 +0300

    Updates

---
+++ precache-manifest.beca2b1b4a70337a83171c82024ee82d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0244292a91395739ba1b71b40ed7248f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b4b25d61e44a8e62ba",
+    "url": "/static/css/2.70f3b80d.chunk.css"
+  },
+  {
+    "revision": "c2b4b25d61e44a8e62ba",
+    "url": "/static/js/2.0a473f38.chunk.js"
+  },
+  {
+    "revision": "6a82abcb4112de770c49",
+    "url": "/static/js/main.e6a8c372.chunk.js"
+  },
+  {
+    "revision": "44a05fc2a21e0ac81aa9",
+    "url": "/static/js/runtime-main.3d480ee1.js"
+  }
+]);
