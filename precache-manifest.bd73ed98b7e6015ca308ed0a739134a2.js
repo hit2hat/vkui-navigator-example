5514b8ad754294b257bf12bc0a4bda115ec26f21
hit2hat <hit2hat@icloud.com>
2019-10-15 11:38:01 +0300

    Updates

---
+++ precache-manifest.bd73ed98b7e6015ca308ed0a739134a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b2f53d44deb21e587b1fd480ff2da1",
+    "url": "/vkui-navigator-example/index.html"
+  },
+  {
+    "revision": "3219e199bad9b782cd23",
+    "url": "/vkui-navigator-example/static/css/2.70f3b80d.chunk.css"
+  },
+  {
+    "revision": "3219e199bad9b782cd23",
+    "url": "/vkui-navigator-example/static/js/2.a204cbd9.chunk.js"
+  },
+  {
+    "revision": "6a82abcb4112de770c49",
+    "url": "/vkui-navigator-example/static/js/main.e6a8c372.chunk.js"
+  },
+  {
+    "revision": "54db3b18f86228426062",
+    "url": "/vkui-navigator-example/static/js/runtime-main.2aaaf5e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beca2b1b4a70337a83171c82024ee82d.js"
+  "/vkui-navigator-example/precache-manifest.bd73ed98b7e6015ca308ed0a739134a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vkui-navigator-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
