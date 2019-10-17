abe81d1c73d49f9720420fe05b23a671164daffc
hit2hat <hit2hat@icloud.com>
2019-10-17 17:26:15 +0300

    Updates

--- precache-manifest.04f32ac8e58ba08e473ce513afab0c3e.js
+++ precache-manifest.04f32ac8e58ba08e473ce513afab0c3e.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "acb3aadc55794ecbe19d0c95e495201e",
+    "revision": "1725a99281039be2efb8d2f64081c2c9",
     "url": "/vkui-navigator-example/index.html"
   },
   {
-    "revision": "b1e0bfd68c41d9a6df7e",
+    "revision": "abaa58a6232ed5458c27",
     "url": "/vkui-navigator-example/static/css/2.70f3b80d.chunk.css"
   },
   {
-    "revision": "b1e0bfd68c41d9a6df7e",
-    "url": "/vkui-navigator-example/static/js/2.298fdccd.chunk.js"
+    "revision": "abaa58a6232ed5458c27",
+    "url": "/vkui-navigator-example/static/js/2.38915238.chunk.js"
   },
   {
     "revision": "b46efda0589cf62be47f",
